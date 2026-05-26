e8f4dcb4aeff2ba54347c053d284acbd1c99d070
Patryk Zybura <patryklogogog@gmail.com>
2026-05-26 22:20:55 +0200

    Dziala :D

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,4 +4,9 @@ public static void main(String[] args) {
 int liczbaA = 5;
 int liczbaB = 10;
 int suma = liczbaA + liczbaB;
-    System.out.println("Wynik dodawania " + liczbaA + " + " + liczbaB + " to: " + suma);}
+    System.out.println("Wynik dodawania " + liczbaA + " + " + liczbaB + " to: " + suma);
+System.out.println("Dziala :DD");
+
+}
+
+
