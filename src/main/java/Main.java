da69ccfe844883f7bb80e69255ce077c98031318
Anastasiia <milahasmail04@gmail.com>
2026-05-26 22:13:49 +0200

    testowałam zmiany w kodzie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
 public static void main(String[] args) {
     System.out.println("test");
-}
+
+int liczbaA = 5;
+int liczbaB = 10;
+int suma = liczbaA + liczbaB;
+    System.out.println("Wynik dodawania " + liczbaA + " + " + liczbaB + " to: " + suma);}
