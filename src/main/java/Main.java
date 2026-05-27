276d4efced2738781f8eb6eb51f35e71dd042970
Patryk Zybura <patryklogogog@gmail.com>
2026-05-27 15:35:20 +0200

    Start projektu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
 public static void main(String[] args) {
-    System.out.println("test");
+    
 
-int liczbaA = 5;
-int liczbaB = 10;
-int suma = liczbaA + liczbaB;
-    System.out.println("Wynik dodawania " + liczbaA + " + " + liczbaB + " to: " + suma);
-System.out.println("Dziala :DD");
-
-System.out.println("Test brancha");
 
 }
-
-
