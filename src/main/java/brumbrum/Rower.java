f7c6cb3d2be9d0001211cfef431cb154a0974b37
whatayoke <290491@student.pwr.edu.pl>
2026-05-30 20:47:39 +0200

    Hierarchia klas "pojazd"

---
+++ src/main/java/brumbrum/Rower.java
@@ -0,0 +1,21 @@
+package brumbrum;
+
+public class Rower extends Pojazd {
+
+    Rower(int wspolrzedna_x, int wspolrzedna_y){
+
+    }
+
+    private void jedz() {
+
+    }
+    @Override
+    public void jedzNastepnaTure(Skrzyzowanie s) {
+
+    }
+    @Override
+    public Boolean czyNaMapie() {
+
+        return true; //chwilowo zeby sie kompilowalo
+    }
+}
