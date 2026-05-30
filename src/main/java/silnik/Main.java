09f597d0bc20d2e39a2cc4b2757ebd3c99dce078
Patryk Zybura <patryklogogog@gmail.com>
2026-05-30 21:38:35 +0200

    Main wyglada calkiem ladnie, poprawka mapy

--- src/main/java/silnik/Main.java
+++ src/main/java/silnik/Main.java
@@ -2,6 +2,7 @@ package silnik;
 
 import drogi.Pojazd;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,40 +13,87 @@ public class Main {
     private int skrzyzowania;
     private int aktualnaTura;
     private List<Pojazd> listaPojazdow;
+    private Mapa mapa;
 
     public Main(){
         pojazdy=0;
         skrzyzowania=0;
         aktualnaTura=0;
+        mapa=null;
 
         listaPojazdow = new ArrayList<>();
     }
 
     public void zacznijSymulacje(){
         System.out.println("Rozpoczeto symulacje.");
+        pojazdy = 10;
+        mapa = GenerowanieSymulacji.stworzMape(20,400);
+        int wymiar = mapa.getWymiar();
+        skrzyzowania = wymiar*wymiar;
+
+        for (int i=0;i<pojazdy;i++){
+            Pojazd p = GenerowanieSymulacji.losujPojazd(wymiar);
+            listaPojazdow.add(p);
+        }
+    }
+
+    public void wykonajTure(Statystyki statystyki){
+        aktualnaTura++;
+        mapa.aktualizujSwiatla();
+        for (int i = 0;i < listaPojazdow.size();i++){
+            Pojazd p = listaPojazdow.get(i);
+            p.jedzNastepnaTure();
+        }
+        usunPojazd();
+
+        statystyki.zbierzDane(listaPojazdow);
+
 
     }
 
-    public void wykonajTure(){}
+    public boolean czyMozliwyRuch(Statystyki statystyki){
+        for (Pojazd p:listaPojazdow){
+            if (mapa.czyWolne(p)){
+                return true;
+            }
+        }
+        System.out.println("Mapa zostala zakorkowana. Koniec symulacji.");
+        statystyki.stworzRaport(aktualnaTura);
+        return false;
+    }
 
-    public boolean czyMozliwyRuch(){return true;}
+    public void usunPojazd(){
+        for (int i= listaPojazdow.size()-1;i>=0;i--){
+            Pojazd p = listaPojazdow.get(i);
 
-    public void usunPojazd(){}
+            if (!p.czyNaMapie()){
+                listaPojazdow.remove(i);
+            }
+        }
 
-    public boolean czyKoniecSymulacji(){return true;}
+    }
 
-    public void zakonczSymulacje(){}
+    public boolean czyKoniecSymulacji(){
+    return listaPojazdow.isEmpty();
+    }
+
+    public void zakonczSymulacje(Statystyki statystyki){
+        System.out.println("Zakonczono symulacje.");
+        statystyki.stworzRaport(aktualnaTura);
+
+    }
 
     public static void main(String[] args) {
 
         Main symulacja = new Main();
+        Statystyki statystyki = new Statystyki();
 
         symulacja.zacznijSymulacje();
 
-        while(!symulacja.czyKoniecSymulacji()){
-            symulacja.wykonajTure();
+        while(!symulacja.czyKoniecSymulacji() && symulacja.czyMozliwyRuch(statystyki)){
+            symulacja.wykonajTure(statystyki);
         }
 
-        symulacja.zakonczSymulacje();
+        symulacja.zakonczSymulacje(statystyki);
     }
 }
