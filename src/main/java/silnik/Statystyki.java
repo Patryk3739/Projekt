db45610a03bec02e195412a9d6090c0482f8ddff
Patryk Zybura <patryklogogog@gmail.com>
2026-06-08 22:24:53 +0200

    Dodanie liczbypojazdow w korku do statystyk + ulepszenie metody czymozliwyruch

--- src/main/java/silnik/Statystyki.java
+++ src/main/java/silnik/Statystyki.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.util.List;
-import pojazdy.Pojazd;
-import pojazdy.Samochod;
-import pojazdy.Rower;
-import pojazdy.Ciezarowka;
+
+import pojazdy.*;
+
 public class Statystyki {
     private String nazwaPliku = "statystyki.csv";
 
@@ -15,14 +14,16 @@ public class Statystyki {
     private int liczbaRowerow;
     private int liczbaCiezarowek;
     private int liczbaLaczna;
+    private int liczbaWKorku;
 
     public Statystyki(){
         liczbaCiezarowek =0;
         liczbaSamochodow=0;
         liczbaRowerow=0;
         liczbaLaczna=0;
+        liczbaWKorku=0;
         try (PrintWriter writer = new PrintWriter(new FileWriter(nazwaPliku))) {
-            writer.println("Tura;Samochody;Ciezarowki;Rowery;Lacznie");
+            writer.println("Tura;Samochody;Ciezarowki;Rowery;W_korku;Lacznie");
         } catch (IOException e) {
             System.err.println("Błąd podczas inicjalizacji pliku CSV: " + e.getMessage());
         }
@@ -32,6 +33,7 @@ public class Statystyki {
         liczbaSamochodow=0;
         liczbaRowerow=0;
         liczbaCiezarowek=0;
+        liczbaWKorku=0;
 
 
     for (Pojazd p:listaPojazdow){
@@ -44,6 +46,10 @@ public class Statystyki {
         if (p instanceof Ciezarowka){
             liczbaCiezarowek++;
         }
+
+        if (p.getStanPojazdu() == StanPojazdu.W_korku) {
+            liczbaWKorku++;
+        }
     }
 
     liczbaLaczna = liczbaCiezarowek+liczbaRowerow+liczbaSamochodow;
@@ -52,7 +58,7 @@ public class Statystyki {
     public void stworzRaport(int aktualnaTura){
         try (PrintWriter writer = new PrintWriter(new FileWriter(nazwaPliku, true))) {
 
-            writer.println(aktualnaTura + ";" + liczbaSamochodow + ";" + liczbaCiezarowek + ";" + liczbaRowerow + ";" + liczbaLaczna);
+            writer.println(aktualnaTura + ";" + liczbaSamochodow + ";" + liczbaCiezarowek + ";" + liczbaRowerow + ";"+ liczbaWKorku +";" + liczbaLaczna);
 
         } catch (IOException e) {
             System.err.println("Błąd podczas zapisu do pliku CSV: " + e.getMessage());
